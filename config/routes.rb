720a87816e0a58e034c4047121404f336a5def8d
jshardy13 <jordan.s.hardy@gmail.com>
2021-12-07 15:38:52 +1000

    fixed bug in schema file and added restful routes for events

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   scope '/api' do
+      get '/events', to: 'events#index'
+      post '/events', to: 'events#create'
+      get '/events/:id', to: 'events#show'
+      put '/events/:id', to: 'events#update'
+      delete '/events/:id', to: 'events#destroy'
+   end
 end
