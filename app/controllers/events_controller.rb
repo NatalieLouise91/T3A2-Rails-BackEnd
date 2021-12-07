8f598d2a5a9e7df5f4d866e234f0dd232c4d3926
jshardy13 <jordan.s.hardy@gmail.com>
2021-12-08 08:59:06 +1000

    updated versioned events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,46 @@
-class EventsController < ApplicationController
-   def index
-      @events = Event.all
-      render json: @events
+module Api::V1
+
+   class EventsController < ApplicationController
+      def index
+         @events = Event.all
+         render json: @events
+      end
+
+      def show
+         @events = Event.find(params[:id])
+         render json: @events
+      end
+
+      def create
+         @Event = Event.create(event_params)
+
+         if @event.save
+            render json: @event, status: :created
+         else 
+            render json: @event.errors, status: :unprocessable_entity
+         end
+      end
+
+      def update
+         @event = Event.find(params[:id])
+         if @event.update(event_params)
+            render json: @event
+         else
+            render json: @event.errors, status: :unprocessable_entity
+         end
+      end
+
+      def destroy
+         @event = Event.find(params[:id])
+         @event.destroy
+      end
+
+      ######################################################
+
+      private
+      def event_params
+         params.permit(:name, :description, :date, :attendees, :location, :time, :contact_name, :contact_phone)
+      end
+      
    end
 end
