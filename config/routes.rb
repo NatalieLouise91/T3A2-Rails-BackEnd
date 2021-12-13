daa89ec3e58a4ee768ad4edc2350608f2d596b4e
jshardy13 <jordan.s.hardy@gmail.com>
2021-12-13 10:32:48 +1000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,16 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  scope '/api' do
-    scope '/auth' do
-      post '/sign_up', to: 'users#create'
-      post '/sign_in', to: 'users#sign_in'
-    end
-  end
-
-   scope '/api' do
-    
-      get '/events', to: 'events#index'
-      # Should the create route url be '/events/create ?
-      post '/events', to: 'events#create'
-      get '/events/:id', to: 'events#show'
-      put '/events/:id', to: 'events#update'
-      delete '/events/:id', to: 'events#destroy'
-     
-     scope '/auth' do
-      post '/sign_up', to: 'users#create'
-      post '/sign_in', to: 'users#sign_in'
-       
-    end
-   end
+scope '/api' do
+    get '/events', to: 'events#index'
+    post '/events', to: 'events#create'
+    get '/events/user',to:'events#my_events'
+    get '/events/:id', to: 'events#show'
+    put 'events/:id', to: 'events#update'
+    delete 'events/:id', to: 'events#destroy'
+scope '/auth' do 
+  post '/sign_up', to: 'users#create'
+  post '/sign_in', to: 'users#sign_in'
 end
+end
+end
+
