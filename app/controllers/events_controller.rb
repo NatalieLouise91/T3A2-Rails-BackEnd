a7faef451c5a1e5a67d4c14ef2b400e2f86eeb1f
jshardy13 <jordan.s.hardy@gmail.com>
2021-12-13 14:09:14 +1000

    updated events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,8 +18,8 @@ class EventsController < ApplicationController
     end
 
     def create
-        #@event = event.create(event_params)
-        @event = current_user.events.create(event_params)
+        @event = event.create(event_params)
+      #   @event = current_user.events.create(event_params)
         if @event.errors.any?
             render json: @event.errors, status: :unprocessable_entity
          else
@@ -69,66 +69,9 @@ class EventsController < ApplicationController
             end
         end
     end
-    private
-    def event_params
-        params.require(:event).permit(:m_text)
-    end
+      private
+   def event_params
+      params.permit(:name, :description, :date, :attendees, :location, :time, :contact_name, :contact_phone)
+   end
 end
-
-
-
-# class EventsController < ApplicationController
-#    before_action :authenticate_user, except: [:index, :show] 
-#     before_action :find_event, only: [:show, :update, :destroy]
-#     before_action :check_ownership, only: [:destroy, :update] 
-    
-#    def index
-#       @events = Event.all
-#       render json: @events
-#    end
-
-#    def show
-#       render json: @event
-#    end 
-
-#     def find_event
-#         begin
-#             @event = Event.find(params[:id])
-#         rescue
-#             render json: {error: "Event does not exist"}, status: 404
-#         end
-#     end
-
-#    def create
-#       @Event = Event.create(event_params)
-
-#       if @event.save
-#          render json: @event, status: :created
-#       else 
-#          render json: @event.errors, status: :unprocessable_entity
-#       end
-#    end
-
-#    def update
-#       @event = Event.find(params[:id])
-#       if @event.update(event_params)
-#          render json: @event
-#       else
-#          render json: @event.errors, status: :unprocessable_entity
-#       end
-#    end
-
-#    def destroy
-#       @event = Event.find(params[:id])
-#       @event.destroy
-#    end
-
-#    ######################################################
-
-#    private
-#    def event_params
-#       params.permit(:name, :description, :date, :attendees, :location, :time, :contact_name, :contact_phone)
-#    end
-
-# end
 
