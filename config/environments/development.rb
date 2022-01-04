17749e2cfc9d99563da53bdc5865187ed3cf9514
Dung Nguyen <nddung2812@gmail.com>
2022-01-05 00:04:00 +1000

    add config.host to the site

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,4 @@
 require "active_support/core_ext/integer/time"
-Rails.application.configure do
-  config.hosts.clear
-end
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -66,5 +63,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  
+  config.hosts << "skemi.herokuapp.com"
 end
