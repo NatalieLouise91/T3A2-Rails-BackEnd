9716836ea54521b448e9745b9506344e3975fd3d
Dung Nguyen <nddung2812@gmail.com>
2022-01-04 23:38:55 +1000

    add config.host.clear to the site

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts.clear
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -63,6 +63,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "skemi.herokuapp.com"
-  config.hosts << /[a-z0-9]+\.c9users\.io/
+  
 end
