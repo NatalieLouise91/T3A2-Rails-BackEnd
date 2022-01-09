c8853463da2be76fd8f01c6b6dc851ae79cf7b42
Dung Nguyen <nddung2812@gmail.com>
2022-01-09 21:13:46 +1000

    done event testing

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+   @user
+
+   before(:all) do
+      @user = User.create!(
+         email: "eric123@test.com", password: "password1", password_confirmation: "password1", first_name: "Johnny", last_name: "Nguyen", phone: 000000, admin: true
+      )
+   end
+
+   after(:all) do
+      User.delete(@user.id)
+   end
+
    it "has a name" do
-      event = Event.new(
+      event = Event.create(
          name: '',
          description: "A valid description",
          date: "05-10-2022",
@@ -10,14 +22,13 @@ RSpec.describe Event, type: :model do
          location: "Test location",
          time: "10pm",
          contact_name: "test-contact-name",
-         contact_phone: 1
-
+         contact_phone: 1,
+         user_id: @user.id
       )
       expect(event).to_not be_valid
-
       event.name = 'Test name'
       expect(event).to be_valid
-
+      expect(event.name).to eq('Test name')
    end
 
    it 'has a description' do
@@ -29,13 +40,13 @@ RSpec.describe Event, type: :model do
          location: "Test location",
          time: "10pm",
          contact_name: "test-contact-name",
-         contact_phone: 1
-
+         contact_phone: 1,
+         user_id: @user.id
       )
       expect(event).to_not be_valid
-
       event.description = 'test-description'
       expect(event).to be_valid
+      expect(event.description).to eq 'test-description'
    end
    
    it 'has a date' do
@@ -47,13 +58,13 @@ RSpec.describe Event, type: :model do
          location: "Test location",
          time: "10pm",
          contact_name: "test-contact-name",
-         contact_phone: 1
-
+         contact_phone: 1,
+         user_id: @user.id
       )
       expect(event).to_not be_valid
-
       event.date = 'test-date'
-      expect(event).to be_valid   
+      expect(event).to be_valid
+      expect(event.date).to eq 'test-date'
    end
 
    it 'has attendees' do
@@ -65,14 +76,15 @@ RSpec.describe Event, type: :model do
          location: "Test location",
          time: "10pm",
          contact_name: "test-contact-name",
-         contact_phone: 1
-
+         contact_phone: 1,
+         user_id: @user.id
       )
       expect(event).to_not be_valid
-
       event.attendees = 1
       expect(event).to be_valid
+      expect(event.attendees).to eq 1
    end
+
    it 'has a location' do
       event = Event.new(
          name: 'test-name',
@@ -82,14 +94,15 @@ RSpec.describe Event, type: :model do
          location: "",
          time: "10pm",
          contact_name: "test-contact-name",
-         contact_phone: 1
-
+         contact_phone: 1,
+         user_id: @user.id
       )
       expect(event).to_not be_valid
-
       event.location = 'test location'
       expect(event).to be_valid
+      expect(event.location).to eq 'test location'
    end
+
    it 'has a time' do
       event = Event.new(
          name: 'test-name',
@@ -99,14 +112,15 @@ RSpec.describe Event, type: :model do
          location: "Test location",
          time: "",
          contact_name: "test-contact-name",
-         contact_phone: 1
-
+         contact_phone: 1,
+         user_id: @user.id
       )
       expect(event).to_not be_valid
-
       event.time = 'test-time'
       expect(event).to be_valid
+      expect(event.time).to eq 'test-time'
    end
+
    it 'has a contact name' do
       event = Event.new(
          name: 'test-name',
@@ -116,14 +130,15 @@ RSpec.describe Event, type: :model do
          location: "Test location",
          time: "10pm",
          contact_name: "",
-         contact_phone: 1
-
+         contact_phone: 1,
+         user_id: @user.id
       )
       expect(event).to_not be_valid
-
       event.contact_name = 'test-contact-name'
       expect(event).to be_valid
+      expect(event.contact_name).to eq 'test-contact-name'
    end
+
    it 'has a contact_phone' do
       event = Event.new(
          name: 'test-name',
@@ -133,13 +148,13 @@ RSpec.describe Event, type: :model do
          location: "Test location",
          time: "10pm",
          contact_name: "test-contact-name",
-         contact_phone: ''
-
+         contact_phone: '',
+         user_id: @user.id
       )
       expect(event).to_not be_valid
-
       event.contact_phone = 1
       expect(event).to be_valid
+      expect(event.contact_phone).to eq 1
    end
 end
 
