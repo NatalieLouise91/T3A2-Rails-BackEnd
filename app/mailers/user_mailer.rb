27e8dfbfbae2b8d5589f6adcd98450b12b6e9f63
Dung Nguyen <nddung2812@gmail.com>
2022-01-16 20:10:37 +1000

    remove @syntax in user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ApplicationMailer
-
-   # method to send email to user
-   def password_reset(@user)
+  # method to send email to user
+  def password_reset(user)
     @user = user
     mail to: user.email, subject: "Password Reset"
   end
